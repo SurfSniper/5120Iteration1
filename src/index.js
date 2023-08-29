0d13fc2de3079f3e491ee36b3ec88355c3ef734a
limbo <447150931@qq.com>
2023-08-30 02:49:40 +0800

    fix: Integrate React

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+//import { RouterProvider } from 'react-router-dom'
+//import { globalRouters } from './router'
 import App from './App';
+
+//import { ConfigProvider } from 'antd'
+//引入Ant Design中文语言包
+//import zhCN from 'antd/locale/zh_CN'
+//全局样式
+//import './common/styles/frame.styl'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
+// root.render(
+//     // <ConfigProvider locale={zhCN}>
+//     //     <RouterProvider router={globalRouters} />
+//     // </ConfigProvider>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
