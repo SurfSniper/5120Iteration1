5cec484edeaa8553303a17a3054acee3e0adf55f
limbo80 <ltm18256453941>
2023-08-30 18:21:20 +0800

    feat: Adjust Style

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,42 +1,25 @@
 import './header.styl'
-
 function Header() {
-
     return (
 
         <header className="text-center" name="home">
             <nav id="menu" className="navbar navbar-default navbar-fixed-top">
-                <div className="container">
-                    <div className="navbar-header">
+                <span className="container">
+                    <span className="navbar-header">
 
                         <a className="navbar-brand" href="index.html"><i className="fa fa-sun-o"></i>Recycle
                             Nest<strong></strong></a>
-                        <ul className="nav navbar-nav navbar-right">
-                            <li><a href="#home" className="page-scroll">Home</a></li>
-                            <li><a href="#services-section" className="page-scroll">Services</a></li>
-                            <li><a href="#works-section" className="page-scroll">Portfolio</a></li>
-                            <li><a href="#about-section" className="page-scroll">About</a></li>
-                            <li><a href="#contact-section" className="page-scroll">Contact</a></li>
+
+                        <ul className="nav navbar-nav navbar-right ul-right">
+                            <li className="li-right"><a href="#home" className="page-scroll">Home</a></li>
+                            <li className="li-right"><a href="#services-section" className="page-scroll">Services</a></li>
+                            <li className="li-right"><a href="#works-section" className="page-scroll">Portfolio</a></li>
+                            <li className="li-right"><a href="#about-section" className="page-scroll">About</a></li>
+                            <li className="li-right"><a href="#contact-section" className="page-scroll">Contact</a></li>
                         </ul>
-
-                    </div>
-
-
-                </div>
+                    </span>
+                </span>
             </nav>
-            <div className="intro-text">
-                <h1>Welcome to <strong>Recycle Nest</strong></h1>
-
-                <div className="focus">
-                    <p>We focus on garbage collection</p>
-                </div>
-
-                <div className="learn-more">
-                    <a href="#services-section" className="btn btn-default btn-lg page-scroll">LEARN MORE
-                    </a>
-                </div>
-            </div>
-
         </header>
 
     )
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,6 +3,7 @@ import {Button} from 'antd'
 import Header from '../../components/header'
 import './home.styl'
 import { goto } from '../../api'
+import React from "react";
 
 function Home() {
 
@@ -10,20 +11,36 @@ function Home() {
     const navigate = useNavigate()
 
     return (
+
+
         <div className="P-home">
             <Header />
 
-            <div className="ipt-con">
-                <Button onClick={() => {
-                    goto('/login')
-                }}>组件外跳转</Button>
+            <div className="intro-text">
+                <h1>Welcome to <strong>Recycle Nest</strong></h1>
+
+                <div className="focus">
+                    <p>We focus on garbage collection</p>
+                </div>
+
+                <div className="learn-more">
+                    <a href="#services-section" className="btn btn-default btn-lg page-scroll">LEARN MORE
+                    </a>
+                </div>
+
+                <div className="ipt-con">
+                    <Button onClick={() => {
+                        goto('/login')
+                    }}>组件外跳转</Button>
+                </div>
+
+                <div className="ipt-con">
+                    <Button type="primary" onClick={() => {
+                        navigate('/login')
+                    }}>返回登录</Button>
+                </div>
             </div>
 
-            <div className="ipt-con">
-                <Button type="primary" onClick={() => {
-                    navigate('/login')
-                }}>返回登录</Button>
-            </div>
         </div>
     )
 }
